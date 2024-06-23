338800dd905ccd69202583e9b79e71bf84f2f196
VixitaPatel <vixitabhalodiya@gmail.com>
2024-06-23 13:34:54 +0530

    some changes

--- Server/Database/connection.js
+++ Server/Database/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const con = await mongoose.connect("mongodb+srv://vixitabhalodiya:mKzfN3PPuHaPF4Xm@cluster0.komdgte.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+        const con = await mongoose.connect("mongodb+srv://vixitabhalodiya:9reIE2MK5dNUrX9T@cluster0.komdgte.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
             useNewUrlParser: true,
             useUnifiedTopology : true,
             // useFindAndModify : false,
